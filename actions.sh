d79251836eff50dba9021b164314695bf4227570
louis gueye <louis.gueye@gmail.com>
2015-02-12 01:19:01 +0100

    work in progress: templating db-upgrader, index-upgrader, tests

--- actions.sh
+++ actions.sh
@@ -3,5 +3,7 @@ cd digitalocean && ./doapi.sh && cd -
 # 2. edit spin up script and configure your capacity with data from
 ansible-playbook -i /etc/ansible/hosts spinup-cluster.yml -e "target_env=test"
 # edit inventory with IPs found on digital ocean
+# Remove hosts from known-hosts list because digital ocean reuses IPS
+ ssh-keygen -f "/home/louis/.ssh/known_hosts" -R <IP>
 # configure playbook
 ansible-playbook -i inventory configure-cluster.yml -e "db_admin_password=" -e "db_user_password=s3cr3t"
